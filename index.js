b26bfc52bbaf1710de756032d6c153810e6df978
Paul S Michalek <psmichalek@gmail.com>
2017-02-04 13:57:21 -0600

    updates

--- index.js
+++ index.js
@@ -52,9 +52,7 @@ MongoClient.connect(DB_URL, (err,database) => {
 })
 
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({extended: true}))
-
 app.use(express.static(path.join(__dirname,'public')))
 
 app.set('view engine','ejs')
